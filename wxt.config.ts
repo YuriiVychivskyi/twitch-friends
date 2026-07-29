e94335d5ea082b5822a5623f0fe8b3ccac79ac9c
Yurii Vychivskyi <yuravychivskii@gmail.com>
2026-07-29 16:01:51 +0100

    feat: add Twitch Friends extension icons

--- wxt.config.ts
+++ wxt.config.ts
@@ -12,6 +12,18 @@ export default defineConfig({
     return {
       name: 'Twitch Friends',
       description: 'Share your current Twitch stream with trusted friends.',
+      icons: {
+        16: 'icons/icon-16.png',
+        32: 'icons/icon-32.png',
+        48: 'icons/icon-48.png',
+        128: 'icons/icon-128.png',
+      },
+      action: {
+        default_icon: {
+          16: 'icons/icon-16.png',
+          32: 'icons/icon-32.png',
+        },
+      },
       permissions: manifestVersion === 2 ? ['storage', ...backendHosts] : ['storage'],
       ...(manifestVersion === 3
         ? {
